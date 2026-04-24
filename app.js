3d236278c23bd0696beb12277e6aab64317c1842
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2026-04-24 11:10:36 -0400

    adding volumes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from Docker server 🚀");
+  res.end("Hello from Volu");
 });
 
 server.listen(3000, () => {
