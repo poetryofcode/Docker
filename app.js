ec3ed017bbc61a550ce3980725aceb1e1afc98a4
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2026-04-30 09:37:23 -0400

    volumes

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
+const crypto = require("crypto");
+global.crypto = crypto;
+
 const http = require("http");
 const { MongoClient } = require("mongodb");
 
-const url = "mongodb://db:27017"; // 👈 important
+const url = "mongodb://db:27017";
 const client = new MongoClient(url);
 
 async function start() {
   await client.connect();
   console.log("Connected to MongoDB");
 
-  const server = http.createServer((req, res) => {
-    res.end("App + DB working 🚀");
+  const db = client.db("testdb");
+  const collection = db.collection("messages");
+
+  const server = http.createServer(async (req, res) => {
+    if (req.url === "/add") {
+      await collection.insertOne({ text: "Hello from Docker 🚀" });
+      res.end("Data added");
+      return;
+    }
+
+    if (req.url === "/list") {
+      const data = await collection.find().toArray();
+      res.end(JSON.stringify(data));
+      return;
+    }
+
+    res.end("Use /add or /list");
   });
 
   server.listen(3000, () => {
