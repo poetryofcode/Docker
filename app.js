749bd20dfa9c85b0e019a1f3970febb6c9f671c6
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2026-04-30 08:45:31 -0400

    compose

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const http = require("http");
+const { MongoClient } = require("mongodb");
 
-const server = http.createServer((req, res) => {
-  res.end("Hello from Volu");
-});
+const url = "mongodb://db:27017"; // 👈 important
+const client = new MongoClient(url);
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+async function start() {
+  await client.connect();
+  console.log("Connected to MongoDB");
+
+  const server = http.createServer((req, res) => {
+    res.end("App + DB working 🚀");
+  });
+
+  server.listen(3000, () => {
+    console.log("Server running on port 3000");
+  });
+}
+
+start();
